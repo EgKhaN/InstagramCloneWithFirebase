9840e8f5bc9ddd4f3d2b13c1c279b007981a6e90
egkhan <Egka61git>
2017-07-24 23:27:28 +0300

    - Project initial structure is done - Bottom and tab menus are finished

---
+++ app/src/main/java/com/egkhan/instagramclonewithfirebase/Home/HomeActivity.java
@@ -0,0 +1,61 @@
+package com.egkhan.instagramclonewithfirebase.Home;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.egkhan.instagramclonewithfirebase.R;
+import com.egkhan.instagramclonewithfirebase.Utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class HomeActivity extends AppCompatActivity {
+    private static final String TAG = "HomeActivity";
+    public static final int ACTIVITY_NUM =0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Log.d(TAG, "onCreate: starting");
+
+        setupBottomNavigationView();
+        setupViewPager();
+    }
+
+    /**
+     * Responsible for adding the 3 tabs : Camera,Home,Messages
+     */
+    private void setupViewPager(){
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CameraFragment());
+        adapter.addFragment(new HomeFragment());
+        adapter.addFragment(new MessagesFragment());
+
+        ViewPager viewPager = (ViewPager)findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_instagram);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
+    }
+
+    /**
+     * Bottom NavigationView setup
+     */
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomnavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(this,bottomNavigationViewEx);
+        //activity değişince ,alakalı menu de seçili olsun diye
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+    }
+}
---
+++ app/src/main/java/com/egkhan/instagramclonewithfirebase/Search/SearchActivity.java
@@ -0,0 +1,42 @@
+package com.egkhan.instagramclonewithfirebase.Search;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.egkhan.instagramclonewithfirebase.R;
+import com.egkhan.instagramclonewithfirebase.Utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+/**
+ * Created by EgK on 7/24/2017.
+ */
+
+public class SearchActivity extends AppCompatActivity {
+    private static final String TAG = "SearchActivity";
+    public static final int ACTIVITY_NUM =1;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Log.d(TAG, "onCreate: started");
+
+        setupBottomNavigationView();
+    }
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomnavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(this,bottomNavigationViewEx);
+
+        //activity değişince ,alakalı menu de seçili olsun diye
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+
+    }
+}
